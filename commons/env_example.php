b3faaac6724df37118850908240463926f27cc71
Leduyduc9632004 <leduyduc963@gmail.com>
2024-06-20 14:18:46 +0700

    thêm file .gitignore

---
+++ commons/env_example.php
@@ -0,0 +1,13 @@
+<?php
+
+// khai báo các biến môi trường
+
+define('PATH_CONTROLLER',__DIR__.'/../controllers/');
+define('PATH_MODEL',__DIR__.'/../models/');
+define('PATH_VIEW',__DIR__.'/../views/');
+
+define('DB_HOST','localhost');
+define('DB_PORT','3306');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','mvc_basic');
